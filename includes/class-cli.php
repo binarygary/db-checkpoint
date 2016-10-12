f7f1a16ca556d821694322aa277b255ce11fc4fb
Gary Kovar <gkovar@gmail.com>
2016-10-11 20:43:40 -0400

    removed logging; still erroring

--- includes/class-cli.php
+++ includes/class-cli.php
@@ -59,13 +59,9 @@ class DBCP_Cli {
 	 */
 	public function checkpoint_save( $args ) {
 
-		error_log( print_r( $args, true ) );
-
 		$upload_dir = wp_upload_dir();
 
 		$location = $upload_dir['basedir'].'/checkpoint-storage/' . time() . '.' . $args[0] . '.sql';
-
-		//error_log($location);
 
 		$db = new DB_Command;
 		$db->export( $location, null );
