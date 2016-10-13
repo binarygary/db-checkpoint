68dbb1f3d15f7595b6ce18e212557978d9576324
Gary Kovar <gkovar@gmail.com>
2016-10-13 15:08:23 -0400

    moved to 1 big 'ol class

--- db-checkpoint.php
+++ db-checkpoint.php
@@ -1,220 +1,198 @@
 <?php
-function db_checkpoint_autoload_classes( $class_name ) {
-	if ( 0 !== strpos( $class_name, 'DBCP_' ) ) {
-		return;
-	}
 
-	$filename = strtolower( str_replace(
-		'_', '-',
-		substr( $class_name, strlen( 'DBCP_' ) )
-	) );
+if ( defined( 'WP_CLI' ) && WP_CLI ) {
 
-	DB_CheckPoint::include_file( 'includes/class-' . $filename );
-}
+	class DB_CheckPoint extends WP_CLI_COMMAND {
 
-spl_autoload_register( 'db_checkpoint_autoload_classes' );
-
-/**
- * Main initiation class
- *
- * @since  NEXT
- */
-final class DB_CheckPoint extends WP_CLI_COMMAND {
-
-	/**
-	 * Current version
-	 *
-	 * @var  string
-	 * @since  NEXT
-	 */
-	const VERSION = '0.1.0';
-
-	/**
-	 * Singleton instance of plugin
-	 *
-	 * @var DB_CheckPoint
-	 * @since  NEXT
-	 */
-	protected static $single_instance = null;
-
-	/**
-	 * Instance of DBCP_Cli
-	 *
-	 * @since NEXT
-	 * @var DBCP_Cli
-	 */
-	protected $cli;
-
-	/**
-	 * Creates or returns an instance of this class.
-	 *
-	 * @since  NEXT
-	 * @return DB_CheckPoint A single instance of this class.
-	 */
-	public static function get_instance() {
-		if ( null === self::$single_instance ) {
-			self::$single_instance = new self();
-		}
-
-		return self::$single_instance;
-	}
-
-	/**
-	 * Sets up our plugin
-	 *
-	 * @since  NEXT
-	 */
-	protected function __construct() {
-		$this->init();
-	}
-
-	/**
-	 * Attach other plugin classes to the base plugin class.
-	 *
-	 * @since  NEXT
-	 * @return void
-	 */
-	public function plugin_classes() {
-		// Attach other plugin classes to the base plugin class.
-		$this->cli = new DBCP_Cli( $this );
-	} // END OF PLUGIN CLASSES FUNCTION
-
-	/**
-	 * Init hooks
-	 *
-	 * @since  NEXT
-	 * @return void
-	 */
-	public function init() {
-		if ( $this->check_requirements() ) {
-			$this->plugin_classes();
+		public function __construct() {
 			$this->load_commands();
 		}
-	}
 
-	/**
-	 * Check if the plugin meets requirements and
-	 * disable it if they are not present.
-	 *
-	 * @since  NEXT
-	 * @return boolean result of meets_requirements
-	 */
-	public function check_requirements() {
-
-		$upload_dir = wp_upload_dir();
-		if ( ! file_exists( $upload_dir[ 'basedir' ] . '/checkpoint-storage' ) ) {
-			mkdir( $upload_dir[ 'basedir' ] . '/checkpoint-storage' );
+		public function load_commands() {
+			WP_CLI::add_command( 'dbsnap', $this - checkpoint_save(), $this->get_checkpoint_save_args() );
+			WP_CLI::add_command( 'dbsnapback', $this->checkpoint_restore(), $this->get_checkpoint_restore_args() );
 		}
 
-		if ( !defined( 'WP_CLI' )) {
+
+		public function get_checkpoint_save_args() {
+			return array(
+				'shortdesc' => 'Restores the checkpoint image of the database.',
+				'synopsis'  => array(
+					array(
+						'type'     => 'positional',
+						'name'     => 'name',
+						'optional' => true,
+						'multiple' => false,
+					),
+				),
+				'when'      => 'before_wp_load',
+			);
+		}
+
+
+		public function get_checkpoint_restore_args() {
+			return array(
+				'shortdesc' => 'Creates a simple checkpoint image of the database.',
+				'synopsis'  => array(
+					array(
+						'type'     => 'positional',
+						'name'     => 'name',
+						'optional' => true,
+						'multiple' => false,
+					),
+				),
+				'when'      => 'before_wp_load',
+			);
+		}
+
+		/**
+		 * Saves a checkpoint of the db.
+		 *
+		 * @author Gary Kovar
+		 *
+		 * @since  0.1.0
+		 */
+		public function checkpoint_save( $args ) {
+
+			$snapshot_name = $this->get_snapshot_name( $args );
+
+			$this->maybe_nuke_checkpoints( $snapshot_name );
+
+			$upload_dir = wp_upload_dir();
+
+			$location  = $upload_dir[ 'basedir' ] . '/checkpoint-storage/' . $snapshot_name . '.' . $this->human_timestamp() . '.sql';
+			$args[ 0 ] = $location;
+
+			$db = new DB_Command;
+			$db->export( $args, null );
+
+			WP_CLI::success( "Checkpoint Saved!" );
+		}
+
+		/**
+		 * Restores the most recent checkpoint of the db.
+		 *
+		 * @author Gary Kovar
+		 *
+		 * @since  0.1.0
+		 */
+		public function checkpoint_restore( $args ) {
+
+			$snapshot_name = $this->get_snapshot_name( $args );
+
+			$upload_dir = wp_upload_dir();
+
+			if ( $restore_file = $this->get_most_recent_file( $snapshot_name ) ) {
+				$location = $upload_dir[ 'basedir' ] . '/checkpoint-storage/' . $this->get_most_recent_file( $snapshot_name );
+			} else {
+				WP_CLI::error( 'No checkpoint found associated with ' . $snapshot_name );
+			}
+
+			$args[ 0 ] = $location;
+
+			$db = new DB_Command;
+			$db->import( $args, null );
+
+			WP_CLI::success( "Checkpoint Restored!" );
+		}
+
+		/**
+		 * Get the name of the most recent backup file.
+		 *
+		 * @author Gary Kovar
+		 *
+		 * @since  0.1.0
+		 *
+		 * @param $backup_name
+		 *
+		 * @return bool
+		 */
+		public function get_most_recent_file( $backup_name ) {
+
+			$upload_dir = wp_upload_dir();
+			$backupsdir = scandir( $upload_dir[ 'basedir' ] . '/checkpoint-storage/', SCANDIR_SORT_DESCENDING );
+			foreach ( $backupsdir as $backup ) {
+				if ( strpos( $backup, $backup_name ) === 0 ) {
+					return $backup;
+				}
+			}
+
 			return false;
 		}
 
-		return true;
-	}
+		/**
+		 * Figure out what name to use with this file.
+		 *
+		 * @author Gary Kovar
+		 *
+		 * @since  0.1.0
+		 *
+		 * @param $args
+		 *
+		 * @return string
+		 */
+		public function get_snapshot_name( $args ) {
 
-	/**
-	 * Magic getter for our object.
-	 *
-	 * @since  NEXT
-	 *
-	 * @param string $field Field to get.
-	 *
-	 * @throws Exception Throws an exception if the field is invalid.
-	 * @return mixed
-	 */
-	public function __get( $field ) {
-		switch ( $field ) {
-			case 'version':
-				return self::VERSION;
-			case 'cli':
-				return $this->$field;
-			default:
-				throw new Exception( 'Invalid ' . __CLASS__ . ' property: ' . $field );
-		}
-	}
+			if ( key_exists( 0, $args ) ) {
+				return $args[ 0 ];
+			}
 
-	/**
-	 * Include a file from the includes directory
-	 *
-	 * @since  NEXT
-	 *
-	 * @param  string $filename Name of the file to be included.
-	 *
-	 * @return bool   Result of include call.
-	 */
-	public static function include_file( $filename ) {
-		$file = self::dir( $filename . '.php' );
-		if ( file_exists( $file ) ) {
-			return include_once( $file );
+			return sanitize_title( get_option( 'blogname', 'shruggy' ) );
 		}
 
-		return false;
+		/**
+		 * Check to see if the checkpoint name matches the site name, if so remove any checkpoints of the same name.
+		 *
+		 * @author Gary Kovar
+		 *
+		 * @since  0.1.0
+		 *
+		 * @param $checkpoint_name
+		 */
+		public function maybe_nuke_checkpoints( $checkpoint_name ) {
+			$this->nuke_checkpoints( $checkpoint_name );
+		}
+
+		/**
+		 * Deletes all previous checkpoints under the same name.
+		 *
+		 * @author Gary Kovar
+		 *
+		 * @since  0.1.0
+		 *
+		 * @param $checkpoint_name
+		 */
+		public function nuke_checkpoints( $checkpoint_name ) {
+			$upload_dir = wp_upload_dir();
+			$backupsdir = scandir( $upload_dir[ 'basedir' ] . '/checkpoint-storage/', SCANDIR_SORT_DESCENDING );
+			foreach ( $backupsdir as $backup ) {
+				if ( strpos( $backup, $checkpoint_name ) === 0 ) {
+					unlink( $upload_dir[ 'basedir' ] . '/checkpoint-storage/' . $backup );
+				}
+			}
+		}
+
+		/**
+		 * Return a pretty human readable time.
+		 *
+		 * @author Gary Kovar
+		 *
+		 * @since  0.1.0
+		 *
+		 * @return false|string
+		 */
+		public function human_timestamp() {
+			return date( "Ymd-Hi", time() );
+		}
+
 	}
 
-	/**
-	 * This plugin's directory
-	 *
-	 * @since  NEXT
-	 *
-	 * @param  string $path (optional) appended path.
-	 *
-	 * @return string       Directory and path
-	 */
-	public static function dir( $path = '' ) {
-		static $dir;
-		$dir = $dir ? $dir : trailingslashit( dirname( __FILE__ ) );
-
-		return $dir . $path;
+	function db_checkpoint() {
+		$dbcheck = new DB_CheckPoint;
 	}
 
-	public function load_commands() {
-		WP_CLI::add_command( 'dbsnap', array( $this->cli, 'checkpoint_save' ) , $this->get_checkpoint_save_args() );
-		WP_CLI::add_command( 'dbsnapback', array( $this->cli, 'checkpoint_restore' ), $this->get_checkpoint_restore_args() );
-	}
-
-	public function get_checkpoint_save_args() {
-		return array(
-			'shortdesc' => 'Restores the checkpoint image of the database.',
-			'synopsis' => array(
-				array(
-					'type'     => 'positional',
-					'name'     => 'name',
-					'optional' => true,
-					'multiple' => false,
-				),
-			),
-			'when' => 'before_wp_load',
-		);
-	}
-
-
-	public function get_checkpoint_restore_args() {
-		return array(
-			'shortdesc' => 'Creates a simple checkpoint image of the database.',
-			'synopsis' => array(
-				array(
-					'type'     => 'positional',
-					'name'     => 'name',
-					'optional' => true,
-					'multiple' => false,
-				),
-			),
-			'when' => 'before_wp_load',
-		);
-	}
+	db_checkpoint();
 
 }
 
-/**
- * Grab the DB_CheckPoint object and return it.
- * Wrapper for DB_CheckPoint::get_instance()
- *
- * @since  NEXT
- * @return DB_CheckPoint  Singleton instance of plugin class.
- */
-function db_checkpoint() {
-	return DB_CheckPoint::get_instance();
-}
 
-db_checkpoint();
