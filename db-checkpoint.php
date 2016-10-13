2915202ade4a66d7f348a12aad30b9f26873c5a3
Gary Kovar <gkovar@gmail.com>
2016-10-12 22:23:37 -0400

    cleaned up a lot of the scaffolded stuff that is plugin related.

--- db-checkpoint.php
+++ db-checkpoint.php
@@ -1,54 +1,4 @@
 <?php
-/**
- * Plugin Name: DB Snapshot
- * Plugin URI:  https://www.binarygary.com/
- * Description: Extends WP-CLI to include a db snapshot for development purposes.
- * Version:     0.1.0
- * Author:      Gary Kovar
- * Author URI:  https://www.binarygary.com/
- * Donate link: https://www.binarygary.com/
- * License:     GPLv2
- * Text Domain: db-snapshot
- * Domain Path: /languages
- *
- * @link    https://www.binarygary.com/
- *
- * @package DB Snapshot
- * @version 0.1.0
- */
-
-/**
- * Copyright (c) 2016 Gary Kovar (email : plugins@binarygary.com)
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License, version 2 or, at
- * your discretion, any later version, as published by the Free
- * Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- */
-
-/**
- * Built using generator-plugin-wp
- */
-
-
-/**
- * Autoloads files with classes when needed
- *
- * @since  NEXT
- *
- * @param  string $class_name Name of the class being requested.
- *
- * @return void
- */
 function db_checkpoint_autoload_classes( $class_name ) {
 	if ( 0 !== strpos( $class_name, 'DBCP_' ) ) {
 		return;
@@ -78,30 +28,6 @@ final class DB_CheckPoint {
 	 * @since  NEXT
 	 */
 	const VERSION = '0.1.0';
-
-	/**
-	 * URL of plugin directory
-	 *
-	 * @var string
-	 * @since  NEXT
-	 */
-	protected $url = '';
-
-	/**
-	 * Path of plugin directory
-	 *
-	 * @var string
-	 * @since  NEXT
-	 */
-	protected $path = '';
-
-	/**
-	 * Plugin basename
-	 *
-	 * @var string
-	 * @since  NEXT
-	 */
-	protected $basename = '';
 
 	/**
 	 * Singleton instance of plugin
@@ -139,9 +65,7 @@ final class DB_CheckPoint {
 	 * @since  NEXT
 	 */
 	protected function __construct() {
-		$this->basename = plugin_basename( __FILE__ );
-		$this->url      = plugin_dir_url( __FILE__ );
-		$this->path     = plugin_dir_path( __FILE__ );
+		$this->init();
 	}
 
 	/**
@@ -156,41 +80,6 @@ final class DB_CheckPoint {
 	} // END OF PLUGIN CLASSES FUNCTION
 
 	/**
-	 * Add hooks and filters
-	 *
-	 * @since  NEXT
-	 * @return void
-	 */
-	public function hooks() {
-		// Priority needs to be:
-		// < 10 for CPT_Core,
-		// < 5 for Taxonomy_Core,
-		// 0 Widgets because widgets_init runs at init priority 1.
-		add_action( 'init', array( $this, 'init' ), 0 );
-	}
-
-	/**
-	 * Activate the plugin
-	 *
-	 * @since  NEXT
-	 * @return void
-	 */
-	public function _activate() {
-		// Make sure any rewrite functionality has been loaded.
-		flush_rewrite_rules();
-	}
-
-	/**
-	 * Deactivate the plugin
-	 * Uninstall routines should be in uninstall.php
-	 *
-	 * @since  NEXT
-	 * @return void
-	 */
-	public function _deactivate() {
-	}
-
-	/**
 	 * Init hooks
 	 *
 	 * @since  NEXT
@@ -198,7 +87,6 @@ final class DB_CheckPoint {
 	 */
 	public function init() {
 		if ( $this->check_requirements() ) {
-			load_plugin_textdomain( 'db-checkpoint', false, dirname( $this->basename ) . '/languages/' );
 			$this->plugin_classes();
 			$this->load_commands();
 		}
@@ -212,16 +100,6 @@ final class DB_CheckPoint {
 	 * @return boolean result of meets_requirements
 	 */
 	public function check_requirements() {
-		if ( ! $this->meets_requirements() ) {
-
-			// Add a dashboard notice.
-			add_action( 'all_admin_notices', array( $this, 'requirements_not_met_notice' ) );
-
-			// Deactivate our plugin.
-			add_action( 'admin_init', array( $this, 'deactivate_me' ) );
-
-			return false;
-		}
 
 		$upload_dir = wp_upload_dir();
 		if ( ! file_exists( $upload_dir[ 'basedir' ] . '/checkpoint-storage' ) ) {
@@ -233,47 +111,6 @@ final class DB_CheckPoint {
 		}
 
 		return true;
-	}
-
-	/**
-	 * Deactivates this plugin, hook this function on admin_init.
-	 *
-	 * @since  NEXT
-	 * @return void
-	 */
-	public function deactivate_me() {
-
-		// We do a check for deactivate_plugins before calling it, to protect
-		// any developers from accidentally calling it too early and breaking things.
-		if ( function_exists( 'deactivate_plugins' ) ) {
-			deactivate_plugins( $this->basename );
-		}
-	}
-
-	/**
-	 * Check that all plugin requirements are met
-	 *
-	 * @since  NEXT
-	 * @return boolean True if requirements are met.
-	 */
-	public function meets_requirements() {
-		// Do checks for required classes / functions
-		// function_exists('') & class_exists('').
-		// We have met all requirements.
-		return true;
-	}
-
-	/**
-	 * Adds a notice to the dashboard if the plugin requirements are not met
-	 *
-	 * @since  NEXT
-	 * @return void
-	 */
-	public function requirements_not_met_notice() {
-		// Output our error.
-		echo '<div id="message" class="error">';
-		echo '<p>' . sprintf( __( 'DB CheckPoint is missing requirements and has been <a href="%s">deactivated</a>. Please make sure all requirements are available.', 'db-checkpoint' ), admin_url( 'plugins.php' ) ) . '</p>';
-		echo '</div>';
 	}
 
 	/**
@@ -290,9 +127,6 @@ final class DB_CheckPoint {
 		switch ( $field ) {
 			case 'version':
 				return self::VERSION;
-			case 'basename':
-			case 'url':
-			case 'path':
 			case 'cli':
 				return $this->$field;
 			default:
@@ -332,22 +166,6 @@ final class DB_CheckPoint {
 		$dir = $dir ? $dir : trailingslashit( dirname( __FILE__ ) );
 
 		return $dir . $path;
-	}
-
-	/**
-	 * This plugin's url
-	 *
-	 * @since  NEXT
-	 *
-	 * @param  string $path (optional) appended path.
-	 *
-	 * @return string       URL and path
-	 */
-	public static function url( $path = '' ) {
-		static $url;
-		$url = $url ? $url : trailingslashit( plugin_dir_url( __FILE__ ) );
-
-		return $url . $path;
 	}
 
 	public function load_commands() {
@@ -399,8 +217,4 @@ function db_checkpoint() {
 	return DB_CheckPoint::get_instance();
 }
 
-// Kick it off.
-add_action( 'plugins_loaded', array( db_checkpoint(), 'hooks' ) );
-
-register_activation_hook( __FILE__, array( db_checkpoint(), '_activate' ) );
-register_deactivation_hook( __FILE__, array( db_checkpoint(), '_deactivate' ) );
+db_checkpoint();
